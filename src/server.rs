8c494782542f92f48add8e9b0c49af75fb0488b9
evano <52883968+evanoooo@users.noreply.github.com>
2026-08-28 14:58:16 +0800

    fix(auth): allow all authenticated logged-in users to access audit features

--- src/server.rs
+++ src/server.rs
@@ -1110,15 +1110,14 @@ impl Server {
             }
             if self.args.auth.has_users() {
                 let authorization = headers.get(AUTHORIZATION);
-                let (user, access_paths) = self.args.auth.guard(
+                let (user, _) = self.args.auth.guard(
                     "",
                     &Method::GET,
                     authorization,
                     query_params.get("token"),
                     false,
                 );
-                let is_admin = user.is_some() && access_paths.map(|ap| ap.perm().readwrite()).unwrap_or(false);
-                if !is_admin {
+                if user.is_none() {
                     if authorization.is_none() {
                         self.auth_reject(res)?;
                     } else {
@@ -1716,7 +1715,7 @@ impl Server {
         );
         let readwrite = access_paths.perm().readwrite();
         let allow_audit = self.args.allow_audit
-            && (!self.args.auth.has_users() || (user.is_some() && readwrite));
+            && (!self.args.auth.has_users() || user.is_some());
         let data = IndexData {
             kind: DataKind::Index,
             href,
